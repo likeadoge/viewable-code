eab6cb43e81df3c67dc57317ea9763fc57bcada5
likeadoge <62692265+likeadoge@users.noreply.github.com>
2021-03-05 18:06:57 +0800

    Update reactable.js

--- pubilc/view/framework/reactive/reactable.js
+++ pubilc/view/framework/reactive/reactable.js
@@ -2,8 +2,39 @@ export class Reactable {
 
     #val = null
 
-    #target = null
+    #target = []
 
-    val(){return this.#val}
+    #emitters = []
 
+    #emitter = new Emitter(() => this.#update)
+
+    #transform = () => { }
+
+    val() { return this.#val }
+
+
+    constructor(list) {
+        this.#target = list
+        this.#target.forEach(v => v instanceof Reactable ? v.listen(this.#emitter) : null)
+    }
+
+    #update() {
+        const newVal = this.#transform(...this.#target.map(v => v instanceof Reactable ? v.val() : v))
+        if ((!newVal instanceof Object || newVal === null) && newVal === this.val) {
+            return
+        } else {
+            this.#val = newVal
+            this.#emitters.forEach(v => v.emit())
+        }
+    }
+
+    listen(emitter) {
+        this.#emitters.push(emitter)
+    }
+}
+
+export class Emitter {
+    constructor(cb){this.#cb = cb}
+    #cb = () => { }
+    emit() { this.#cb() }
 }
